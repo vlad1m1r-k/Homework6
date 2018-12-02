8099bb57774f46f5128dadb955420460436d2b41
Vladimir <vova.kvz3@gmail.com>
2018-12-02 14:07:12 +0200

    ""

--- src/ua/kiev/prog/homework6/part5/DirMonitor.java
+++ src/ua/kiev/prog/homework6/part5/DirMonitor.java
@@ -1,16 +1,60 @@
 package ua.kiev.prog.homework6.part5;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class DirMonitor {
+    private static File dirForMon;
+
     public static void main(String[] args) {
         if (args.length < 1) System.out.println("Usage: DirMonitor MonitoringDirectory\nEnter for Exit");
         else {
-            File dirForMon = new File(args[0]);
-            if (!dirForMon.exists() || !dirForMon.isDirectory()) System.out.println("It is not directory.");
+            dirForMon = new File(args[0]);
+            if (!dirForMon.exists() || !dirForMon.isDirectory()) System.out.println("It is not a directory.");
             else {
-
+                Thread thread = new Thread(() -> {
+                    File dirForMon = DirMonitor.getDirForMon();
+                    List<File> previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
+                    File[] newState;
+                    while (true) {
+                        newState = dirForMon.listFiles();
+                        if (!Arrays.equals(previousList.toArray(), newState)) {
+                            for (int i = 0; i < newState.length; i++) {
+                                if (previousList.remove(newState[i])) {
+                                   newState[i] = null;
+                                }
+                            }
+                            for (File file: newState){
+                                if (file != null){
+                                    System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" added.");
+                                }
+                            }
+                            for (File file: previousList){
+                                if (file != null){
+                                    System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" removed.");
+                                }
+                            }
+                            previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
+                        }
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.setDaemon(true);
+                thread.start();
+                System.out.println("Enter for exit.");
+                new Scanner(System.in).nextLine();
             }
         }
+    }
+
+    public static File getDirForMon() {
+        return dirForMon;
     }
 }
