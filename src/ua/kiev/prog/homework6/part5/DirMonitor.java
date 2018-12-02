a42cad049c6b632bb72bf7fc02fc4098185eefd5
Vladimir <vova.kvz3@gmail.com>
2018-12-02 17:29:06 +0200

    ""

--- src/ua/kiev/prog/homework6/part5/DirMonitor.java
+++ src/ua/kiev/prog/homework6/part5/DirMonitor.java
@@ -15,37 +15,7 @@ public class DirMonitor {
             dirForMon = new File(args[0]);
             if (!dirForMon.exists() || !dirForMon.isDirectory()) System.out.println("It is not a directory.");
             else {
-                Thread thread = new Thread(() -> {
-                    File dirForMon = DirMonitor.getDirForMon();
-                    List<File> previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
-                    File[] newState;
-                    while (true) {
-                        newState = dirForMon.listFiles();
-                        if (!Arrays.equals(previousList.toArray(), newState)) {
-                            for (int i = 0; i < newState.length; i++) {
-                                if (previousList.remove(newState[i])) {
-                                   newState[i] = null;
-                                }
-                            }
-                            for (File file: newState){
-                                if (file != null){
-                                    System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" added.");
-                                }
-                            }
-                            for (File file: previousList){
-                                if (file != null){
-                                    System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" removed.");
-                                }
-                            }
-                            previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
-                        }
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                Thread thread = new Thread(() -> startThread());
                 thread.setDaemon(true);
                 thread.start();
                 System.out.println("Enter for exit.");
@@ -56,5 +26,37 @@ public class DirMonitor {
 
     public static File getDirForMon() {
         return dirForMon;
+    }
+
+    private static void startThread(){
+        File dirForMon = DirMonitor.getDirForMon();
+        List<File> previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
+        File[] newState;
+        while (true) {
+            newState = dirForMon.listFiles();
+            if (!Arrays.equals(previousList.toArray(), newState)) {
+                for (int i = 0; i < newState.length; i++) {
+                    if (previousList.remove(newState[i])) {
+                        newState[i] = null;
+                    }
+                }
+                for (File file: newState){
+                    if (file != null){
+                        System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" added.");
+                    }
+                }
+                for (File file: previousList){
+                    if (file != null){
+                        System.out.println((file.isDirectory()?"Directory \"":"File \"") + file.getName() + "\" removed.");
+                    }
+                }
+                previousList = new ArrayList<File>(Arrays.asList(dirForMon.listFiles()));
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
